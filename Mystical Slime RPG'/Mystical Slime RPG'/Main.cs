9081321a2f68aedf3efd7d0e344c4b529513c518
deepu460 <harshavardhan_22@yahoo.com>
2015-11-01 14:34:10 -0600

    Hello, World!

---
+++ Mystical Slime RPG'/Mystical Slime RPG'/Main.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mystical_Slime_RPG_ {
+    class Main {
+        public static void Main() {
+            System.Console.WriteLine("Hello, World!");
+        }
+    }
+}

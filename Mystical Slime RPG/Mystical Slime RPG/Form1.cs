48a4c8b3e4bb1afb5b90f35b90314364dc061b04
Kappacitance <zhaohongsheng1@hotmail.com>
2015-11-01 15:07:18 -0600

    MOTHA OF RNGESUS

--- Mystical Slime RPG/Mystical Slime RPG/Form1.cs
+++ Mystical Slime RPG/Mystical Slime RPG/Form1.cs
@@ -16,5 +16,10 @@ namespace Mystical_Slime_RPG_
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }

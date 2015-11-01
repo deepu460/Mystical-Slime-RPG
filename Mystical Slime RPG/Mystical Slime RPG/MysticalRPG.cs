112bfbe2c6313889d5af9dabf862ca0f440427d3
Kappacitance <zhaohongsheng1@hotmail.com>
2015-11-01 15:26:23 -0600

    NEGRO MEHANIX

--- Mystical Slime RPG/Mystical Slime RPG/MysticalRPG.cs
+++ Mystical Slime RPG/Mystical Slime RPG/MysticalRPG.cs
@@ -5,7 +5,10 @@ namespace Mystical_Slime_RPG_{
     class MysticalRPG {
         public static void Main() {
             Console.WriteLine("Hello, World!");
+            Form1 f = new Form1();
+            f.Show();
             Console.ReadLine();
+
         }
     }
 }

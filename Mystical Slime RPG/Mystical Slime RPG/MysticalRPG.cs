31563336a8e0105f70b0b405885930f2078bf96e
deepu460 <harshavardhan_22@yahoo.com>
2015-11-01 14:51:13 -0600

    Fixed Moar Stuff

--- Mystical Slime RPG/Mystical Slime RPG/MysticalRPG.cs
+++ Mystical Slime RPG/Mystical Slime RPG/MysticalRPG.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Mystical_Slime_RPG_ {
+﻿namespace Mystical_Slime_RPG_{
     class MysticalRPG {
         public static void Main() {
             System.Console.WriteLine("Hello, World!");
